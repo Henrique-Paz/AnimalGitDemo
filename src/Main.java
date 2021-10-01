cc18b670f4eb270267369e171486580146f06817
Henrique <hm.paz@campus.fct.unl.pt>
2021-10-01 10:26:52 +0100

    Adding Super

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
      */
     public class Main {
         public static void main(String[] args) {
-            System.out.println("Animal git demo");
+            System.out.println("Super Animal git demo");
         }
     }
 
